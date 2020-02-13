0cf4a2a73eb36b40e0d9660378701e8c1a01401e
Jelle Leempoels <jelle_leempoels@trimble.com>
2020-02-13 14:24:35 +0100

    test

---
+++ controls/httpd.rb
@@ -0,0 +1,75 @@
+
+describe service('httpd') do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+  its('processes') {should include 'httpd'}
+end
+
+describe port(443) do
+  it { should be_listening }
+  its('processes') {should include 'httpd'}
+end
+
+describe file('/etc/httpd/conf.d/welcome.conf') do
+  it { should_not exist }
+end
+
+describe file('/etc/httpd/conf.d/ssl.conf') do
+  it { should_not exist }
+end
+
+describe file('/etc/httpd/conf.d/autoindex.conf') do
+  it { should_not exist }
+end
+
+
+describe file('/etc/httpd/conf.modules.d/00-base.conf') do
+  it { should exist }
+  its('owner') { should eq 'root' }
+  its('group') { should eq 'root' }
+  its('mode') { should cmp '0644' }
+  its('content') { should match '^#LoadModule info_module modules/mod_info.so' }
+  its('content') { should match '^#LoadModule userdir_module modules/mod_userdir.so' }
+  its('content') { should match '^#LoadModule autoindex_module modules/mod_autoindex.so' }
+end
+
+
+describe http('http://localhost/server-status') do
+  its('status') { should cmp 200 }
+end
+
+describe http('https://localhost/server-status', ssl_verify: false) do
+  its('status') { should cmp 403 }
+end
+
+ip = command("hostname -I").stdout.strip
+
+describe http('http://' +ip +'/server-status') do
+  its('status') { should cmp 403 }
+end
+
+describe http('https://' +ip +'/server-status', ssl_verify: false) do
+  its('status') { should cmp 403 }
+end
+
+
+describe command("httpd -t -D DUMP_MODULES | grep -E '^ http_module'") do
+  its('stdout') { should match '^ http_module' }
+end
+
+describe command("httpd -t -D DUMP_MODULES | grep -E '^ info_module'") do
+  its('stdout') { should_not match '^ info_module' }
+end
+
+describe command("httpd -t -D DUMP_MODULES | grep -E '^ userdir_module'") do
+  its('stdout') { should_not match '^ userdir_module' }
+end
+
+describe command("httpd -t -D DUMP_MODULES | grep -E '^ autoindex_module'") do
+  its('stdout') { should_not match '^ autoindex_module' }
+end
